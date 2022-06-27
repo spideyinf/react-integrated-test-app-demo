98cb1c724666588b335dc1bfe7b9a166c44638e7
Phuc Hoang <phuchoang.inf@gmail.com>
2022-06-27 16:21:34 +0700

    Init base app

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Popover } from '@headlessui/react';
+
+export const Header = () => {
+  return (
+    <Popover className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="/">
+              <span className="sr-only">React Movies App</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <a
+              href="/login"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Login/Register
+            </a>
+          </div>
+        </div>
+      </div>
+    </Popover>
+  );
+};
